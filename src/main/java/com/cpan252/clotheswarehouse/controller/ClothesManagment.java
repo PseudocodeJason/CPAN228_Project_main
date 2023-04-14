22f6d6c4df3a486b0c66ba44b9e35725f12a8665
Jason <jason.tieu@Hotmail.com>
2023-04-14 15:03:10 -0400

    Added CrossOrigin Support for ClothesManagements

--- src/main/java/com/cpan252/clotheswarehouse/controller/ClothesManagment.java
+++ src/main/java/com/cpan252/clotheswarehouse/controller/ClothesManagment.java
@@ -1,15 +1,36 @@
 package com.cpan252.clotheswarehouse.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.cpan252.clotheswarehouse.model.dto.DistributionCentreDto;
 
 @Controller
 @RequestMapping("/admin")
+@CrossOrigin(origins = "http://localhost:8082")
 public class ClothesManagment {
+    private RestTemplate restTemplate;
+ 
+    public ClothesManagment(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping
     public String clothesManagment(){
         return "clothesManagment";
+    }
+
+    @ModelAttribute("centres")
+    public List<DistributionCentreDto> getDistributionCentre(){
+        var centres = restTemplate.getForObject("http://localhost:8082/api/DistributionCentres", DistributionCentreDto[].class);
+        return Arrays.asList(centres);
     }
 }
---
+++ src/main/java/com/cpan252/clotheswarehouse/model/dto/ItemDto.java
@@ -0,0 +1,38 @@
+package com.cpan252.clotheswarehouse.model.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDto {
+    public enum Brand {
+        BALENCIAGA("Balenciaga"), STONEISLAND("Stone Island"), DIOR("Dior"), CHANEL("Chanel");
+        
+        private String brandname;
+
+        private Brand(String brandname) {
+            this.brandname = brandname;
+        }
+
+        public String getBrandname() {
+            return brandname;
+        }
+    }
+
+
+    private Long id;
+    private String name;
+    private Brand brand;
+    private int yearofcreation;
+    private BigDecimal price;
+
+    private int quantity;
+    
+    private LocalDate createdAt = LocalDate.now();
+}
