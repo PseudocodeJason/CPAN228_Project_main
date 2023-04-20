b45f17eec3d5b005f566b1964a3d37ef89ce5f8e
mohamadAlhawameda <alhawameda4@gmail.com>
2023-04-20 00:02:46 -0400

    Fixed add clothes and search button and added a little design

--- src/main/java/com/cpan252/clotheswarehouse/config/WebConfig.java
+++ src/main/java/com/cpan252/clotheswarehouse/config/WebConfig.java
@@ -2,17 +2,27 @@ package com.cpan252.clotheswarehouse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@EnableWebSecurity
+public class WebConfig  implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login");
         registry.addViewController("/").setViewName("add.html");
+        
     }
+    
+
+
 
     @Bean
     public RestTemplate restTemplate(){
