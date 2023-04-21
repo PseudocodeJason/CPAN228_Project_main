c2306983f75a0524c7317a0963825bda461d0730
Jason <jason.tieu@Hotmail.com>
2023-04-21 02:55:16 -0400

    Added Post To deleteItem, Added ModelAttribute centreId

--- src/main/java/com/cpan252/clotheswarehouse/controller/ItemsController.java
+++ src/main/java/com/cpan252/clotheswarehouse/controller/ItemsController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +16,7 @@ import com.cpan252.clotheswarehouse.model.dto.ItemDto;
 
 
 @Controller
-@RequestMapping("/admin/centre/{warehouseId}")
+@RequestMapping("/admin/centre/{centreId}")
 public class ItemsController {
     
     private RestTemplate restTemplate;  
@@ -30,11 +31,20 @@ public class ItemsController {
     }
     
     @ModelAttribute("items")
-    public List<ItemDto> getDistributionCentreId(@PathVariable int warehouseId){
-        var items = restTemplate.getForObject("http://localhost:8082/api/DistributionCentre/" + warehouseId + "/items", 
+    public List<ItemDto> getDistributionCentreId(@PathVariable int centreId){
+        var items = restTemplate.getForObject("http://localhost:8082/api/DistributionCentre/" + centreId + "/items", 
         ItemDto[].class);
         return Arrays.asList(items);
     }
 
-    
+    @ModelAttribute("centreId")
+    public int getWarehouseID(@PathVariable int centreId){
+        return centreId;
+    }
+
+    @PostMapping("/delete/{itemId}")
+    public String deleteItem(@PathVariable int centreId, @PathVariable int itemId){
+        restTemplate.delete("http://localhost:8082/api/DistributionCentre/{warehouseId}/items/{itemId}", centreId, itemId);
+        return "redirect:/admin/centre/" + centreId;
+    }
 }
