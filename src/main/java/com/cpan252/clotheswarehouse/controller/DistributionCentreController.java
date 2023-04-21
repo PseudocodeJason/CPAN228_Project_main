907bcf975c1643665a0b0ae3151af0ab29f8226e
Jason <jason.tieu@Hotmail.com>
2023-04-21 01:36:03 -0400

    Added Delete Warehouse Method

--- src/main/java/com/cpan252/clotheswarehouse/controller/DistributionCentreController.java
+++ src/main/java/com/cpan252/clotheswarehouse/controller/DistributionCentreController.java
@@ -7,13 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.cpan252.clotheswarehouse.model.dto.DistributionCentreDto;
 
 @Controller
-@RequestMapping("/Centres")
+@RequestMapping("/admin/centres")
 @CrossOrigin(origins = "http://localhost:8082")
 public class DistributionCentreController {
     
@@ -33,7 +35,11 @@ public class DistributionCentreController {
         var centres = restTemplate.getForObject("http://localhost:8082/api/DistributionCentre", DistributionCentreDto[].class);
         return Arrays.asList(centres);
     }
-
-
+    
+    @PostMapping("/delete/{centreId}")
+    public String deleteDistributionCentre(@PathVariable int centreId) {
+        restTemplate.delete("http://localhost:8082/api/DistributionCentre/{centreId}", centreId);
+        return "redirect:/admin/centres";
+    }
 
 }
